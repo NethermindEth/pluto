9e0bdeabb131509ab704801d732cd5e5811b29c3
Lautaro Emanuel <emlautarom1@gmail.com>
2025-10-08 12:24:06 -0300

    Add Nix support

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustc
+          cargo
+          rust-analyzer
+          rustfmt
+          clippy
+        ];
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+        shellHook = ''
+          chmod +x .githooks/* && ln -sf $(pwd)/.githooks/* .git/hooks/
+        '';
+      };
+
+    }
+  );
+}
