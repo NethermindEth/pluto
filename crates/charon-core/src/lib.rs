f3cd8e775a40a5359d65a7c0f4e6b9e065321094
Lautaro Emanuel <emlautarom1@gmail.com>
2025-10-22 17:03:47 -0300

    Match module docs

--- crates/charon-core/src/lib.rs
+++ crates/charon-core/src/lib.rs
@@ -1,5 +1,3 @@
 //! Core libraries for the Charon distributed validator client
 
-/// Module `qbft` is an implementation of the <https://arxiv.org/pdf/2002.03613.pdf> paper
-/// referenced by the QBFT spec <https://github.com/ConsenSys/qbft-formal-spec-and-verification>.
 pub mod qbft;
--- crates/charon-core/src/qbft/mod.rs
+++ crates/charon-core/src/qbft/mod.rs
@@ -1,3 +1,15 @@
+//! Package `qbft` is an implementation of the ["The Istanbul BFT Consensus Algorithm"](https://arxiv.org/pdf/2002.03613.pdf) by Henrique Moniz
+//! as referenced by the [QBFT spec](https://github.com/ConsenSys/qbft-formal-spec-and-verification).
+//!
+//! ## Features
+//!
+//! - Simple API, just a single function: `qbft.Run`.
+//! - Consensus on arbitrary data.
+//! - Transport abstracted and not provided.
+//! - Decoupled from process authentication and message signing (not provided).
+//! - No dependencies.
+//! - Explicit justifications.
+
 // TODO: Remove these checks
 #![allow(missing_docs)]
 #![allow(clippy::type_complexity)]
