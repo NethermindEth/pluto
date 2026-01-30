ad2d221c8a8d15742581746c3b54f0d145709654
Quang Le <iamquang95@gmail.com>
2026-01-30 22:51:09 +0700

    feat: Implement eth2util/deposit (#134)
    * feat: implement eth2util/deposit
    
    * feat: use TreeHash
    
    * feat: add eth2util/helpers
    
    * fix: centralize logic and errors
    
    * test: add test marshal_deposit_data
    
    * fix: move dependencies to workspace cargo.toml
    
    * fix: address comments
    
    * fix: add more test to deposit
    
    * fix: move DepositData function to impl DepositData
    
    * fix: define Result type at crate level
    
    * feat: implement epoch_from_slot
    
    * fix: remove error helper functions
    
    * fix: read_deposit_data_files use async file
    
    * test: improve tests on deposit crate
    
    * test: fix test name
    
    * fix: using alloy to deal with Address
    
    * fix: use AsRef
    
    * fix: resolve merge issue

--- crates/eth2util/src/lib.rs
+++ crates/eth2util/src/lib.rs
@@ -1,6 +1,6 @@
-//! # Charon ETH2
+//! # Pluto ETH2
 //!
-//! Ethereum 2.0 integration and utilities for the Charon distributed validator
+//! Ethereum 2.0 integration and utilities for the Pluto distributed validator
 //! node. This crate provides interfaces, types, and utilities for interacting
 //! with Ethereum 2.0 networks and validator operations.
 
@@ -15,6 +15,12 @@ pub mod eip712;
 
 /// Network utilities.
 pub mod network;
+
+/// Deposit data creation and management.
+pub mod deposit;
+
+/// Ethereum address helpers.
+pub mod helpers;
 
 /// Utilities.
 pub(crate) mod utils;
