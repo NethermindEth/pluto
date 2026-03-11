a67baeca88febae7812a6c78cbd214bb82d4a0bd
Lautaro Emanuel <emlautarom1@gmail.com>
2026-03-11 17:31:44 -0300

    Test `clear_data_dir_contains_deposit_data`

--- crates/dkg/src/disk.rs
+++ crates/dkg/src/disk.rs
@@ -383,4 +383,19 @@ mod tests {
             Err(super::DiskError::DataDirNotClean { .. })
         ));
     }
+
+    #[tokio::test]
+    async fn clear_data_dir_contains_deposit_data() {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let data_dir = temp_dir.path();
+        tokio::fs::write(data_dir.join("deposit-data-32eth.json"), [0x0, 0x1, 0x2])
+            .await
+            .unwrap();
+
+        let result = super::check_clear_data_dir(data_dir).await;
+        assert!(matches!(
+            result,
+            Err(super::DiskError::DataDirNotClean { .. })
+        ));
+    }
 }
