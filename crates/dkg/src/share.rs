0952f9a010a69865feb2bf7ea57a15d4efd81b81
Lautaro Emanuel <emlautarom1@gmail.com>
2026-03-13 17:59:17 -0300

    Add `ShareMsg`
    - Impl `From<&Share> for ShareMsg`

--- crates/dkg/src/share.rs
+++ crates/dkg/src/share.rs
@@ -8,3 +8,30 @@ pub(crate) struct Share {
 
     pub public_shares: HashMap<u64, pluto_crypto::types::PublicKey>, // u64 == Share index
 }
+
+/// The [`Share`] message wire format sent by the dealer.
+pub(crate) struct ShareMsg {
+    pub pub_key: Vec<u8>,
+    pub pub_shares: Vec<Vec<u8>>,
+    pub secret_share: Vec<u8>,
+}
+
+impl From<&Share> for ShareMsg {
+    fn from(share: &Share) -> Self {
+        let pub_key = share.pub_key.to_vec();
+        let secret_share = share.secret_share.to_vec();
+
+        // Sort pub shares by id.
+        let pub_shares = {
+            let mut entries: Vec<_> = share.public_shares.iter().collect();
+            entries.sort_by_key(|(id, _)| *id);
+            entries.into_iter().map(|(_, pk)| pk.to_vec()).collect()
+        };
+
+        Self {
+            pub_key,
+            pub_shares,
+            secret_share,
+        }
+    }
+}
