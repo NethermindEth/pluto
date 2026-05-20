1038890a938bc952e459d9c9bb87cd217f3d7f87
Quang Le <iamquang95@gmail.com>
2026-05-20 23:48:14 +0700

    refactor: add ClientRecord

--- crates/core/src/qbft/fake_clock.rs
+++ crates/core/src/qbft/fake_clock.rs
@@ -28,7 +28,13 @@ struct FakeClockInner {
     now: Instant,
     last_id: usize,
     cancelled: bool,
-    clients: BTreeMap<usize, (mpmc::Sender<Instant>, Instant, TimerPriority)>,
+    clients: BTreeMap<usize, ClientRecord>,
+}
+
+struct ClientRecord {
+    sender: mpmc::Sender<Instant>,
+    deadline: Instant,
+    priority: TimerPriority,
 }
 
 impl FakeClock {
@@ -80,7 +86,14 @@ impl FakeClock {
             let deadline = inner.now + duration;
 
             inner.last_id += 1;
-            inner.clients.insert(id, (tx, deadline, priority));
+            inner.clients.insert(
+                id,
+                ClientRecord {
+                    sender: tx,
+                    deadline,
+                    priority,
+                },
+            );
 
             id
         };
@@ -124,9 +137,14 @@ impl FakeClock {
             inner.now += duration;
             let now = inner.now;
 
-            for (&id, (ch, deadline, priority)) in &inner.clients {
-                if *deadline <= now {
-                    expired.push((id, *deadline, *priority, ch.clone()));
+            for (&id, record) in &inner.clients {
+                if record.deadline <= now {
+                    expired.push((
+                        id,
+                        record.deadline,
+                        record.priority,
+                        record.sender.clone(),
+                    ));
                 }
             }
 
