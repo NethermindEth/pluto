8504da10ea5dc093e1a03b947300fc49694c8802
Lautaro Emanuel <emlautarom1@gmail.com>
2026-05-20 15:43:14 -0300

    Refactor
    - Expose top-level `new`
    - Remove impossible errors
    - Add inline docs

--- crates/core/src/aggsigdb/memory.rs
+++ crates/core/src/aggsigdb/memory.rs
@@ -1,37 +1,34 @@
 use crate::types;
 use std::collections::{HashMap, hash_map::Entry};
 
+/// Errors for the in-memory AggSigDB implementation.
 #[derive(Debug, thiserror::Error)]
-enum StoreError {
+pub enum Error {
+    /// Data for the same duty and public key already exists but does not match
+    /// the new data.
     #[error("Mismatching data")]
     MismatchingData,
-
-    #[error("Send error: {0}")]
-    Send(#[from] tokio::sync::mpsc::error::SendError<MemDBCommand>),
-
-    #[error("Recv error: {0}")]
-    Recv(#[from] tokio::sync::oneshot::error::RecvError),
 }
 
 #[derive(Debug)]
-enum MemDBCommand {
+enum Command {
     Store {
         duty: types::Duty,
         pub_key: types::PubKey,
         signed_data: Box<dyn types::SignedData>,
 
-        response: tokio::sync::oneshot::Sender<Result<(), StoreError>>,
+        response: tokio::sync::oneshot::Sender<Result<(), Error>>,
     },
 }
 
 #[derive(Debug)]
-struct MemDBActor {
-    receiver: tokio::sync::mpsc::Receiver<MemDBCommand>,
+struct Actor {
+    receiver: tokio::sync::mpsc::Receiver<Command>,
     data: HashMap<(types::Duty, types::PubKey), Box<dyn types::SignedData>>,
 }
 
-impl MemDBActor {
-    fn new(receiver: tokio::sync::mpsc::Receiver<MemDBCommand>) -> Self {
+impl Actor {
+    fn new(receiver: tokio::sync::mpsc::Receiver<Command>) -> Self {
         Self {
             receiver,
             data: HashMap::new(),
@@ -41,7 +38,7 @@ impl MemDBActor {
     async fn run(&mut self) {
         while let Some(cmd) = self.receiver.recv().await {
             match cmd {
-                MemDBCommand::Store {
+                Command::Store {
                     duty,
                     pub_key,
                     signed_data,
@@ -59,13 +56,13 @@ impl MemDBActor {
         duty: types::Duty,
         pub_key: types::PubKey,
         signed_data: Box<dyn types::SignedData>,
-    ) -> Result<(), StoreError> {
+    ) -> Result<(), Error> {
         // TODO: Add deadline tracking
         // _ = db.deadliner.Add(command.duty)
 
         match self.data.entry((duty, pub_key)) {
             Entry::Occupied(slot) if slot.get().as_ref() != signed_data.as_ref() => {
-                Err(StoreError::MismatchingData)
+                Err(Error::MismatchingData)
             }
             Entry::Occupied(_) => Ok(()),
             Entry::Vacant(slot) => {
@@ -76,43 +73,53 @@ impl MemDBActor {
     }
 }
 
-struct MemDBHandle {
-    sender: tokio::sync::mpsc::Sender<MemDBCommand>,
+/// Handle to interact with the AggSigDB in-memory actor.
+#[derive(Clone)]
+pub struct Handle {
+    sender: tokio::sync::mpsc::Sender<Command>,
 }
 
-impl MemDBHandle {
+impl Handle {
     fn new() -> Self {
         let (sender, receiver) = tokio::sync::mpsc::channel(100);
-        let mut actor = MemDBActor::new(receiver);
-        // TODO: Pass a cancellation token
+        let mut actor = Actor::new(receiver);
         tokio::spawn(async move {
             actor.run().await;
         });
         Self { sender }
     }
 
-    async fn store(
+    /// Stores aggregated signed duty data set.
+    pub async fn store(
         &self,
         duty: types::Duty,
         pub_key: types::PubKey,
         signed_data: Box<dyn types::SignedData>,
-    ) -> Result<(), StoreError> {
+    ) -> Result<(), Error> {
         let (tx, rx) = tokio::sync::oneshot::channel();
-        self.sender
-            .send(MemDBCommand::Store {
+        let _ = self
+            .sender
+            .send(Command::Store {
                 duty,
                 pub_key,
                 signed_data,
                 response: tx,
             })
-            .await?;
-        rx.await?
+            .await;
+        rx.await.expect("Actor task has been killed")
     }
+}
+
+/// Create a new memory AggSigDB implementation and return its handle.
+///
+/// Clone this handle to share access to the same AggSigDB instance across
+/// multiple tasks.
+pub fn new() -> Handle {
+    Handle::new()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use crate::{
         signeddata::SignedDataError,
         types::{Duty, PubKey, Signature, SignedData, SlotNumber},
@@ -137,7 +144,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_single_handle_store() {
-        let handle = MemDBHandle::new();
+        let handle = super::new();
         let duty = Duty::new_attester_duty(SlotNumber::new(1));
         let pub_key = PubKey::new([7u8; 48]);
         let signed_data: Box<dyn SignedData> = Box::new(MockSignedData);
