a98d16f03215324ccfd1f7ea14d47c59864f21f4
Lautaro Emanuel <emlautarom1@gmail.com>
2026-07-29 17:37:03 -0300

    Inline vars

--- crates/cli/src/commands/relay.rs
+++ crates/cli/src/commands/relay.rs
@@ -98,10 +98,8 @@ impl TryInto<pluto_relay_server::config::Config> for RelayArgs {
             }
         };
 
-        let p2p_relay = self.relay.p2p_relay_log_level;
-
         let log_config = build_console_tracing_config(
-            relay_filter(self.log.level.as_str(), &p2p_relay),
+            relay_filter(&self.log.level, &self.relay.p2p_relay_log_level),
             &self.log.color,
             loki_config,
         );
@@ -110,7 +108,7 @@ impl TryInto<pluto_relay_server::config::Config> for RelayArgs {
             .data_dir(self.data_dir.data_dir)
             .http_addr(self.relay.http_address)
             .auto_p2p_key(self.relay.auto_p2p_key)
-            .libp2p_log_level(p2p_relay)
+            .libp2p_log_level(self.relay.p2p_relay_log_level)
             .max_res_per_peer(self.relay.max_res_per_peer)
             .max_conns(self.relay.max_conns)
             // Invert p2p-advertise-private-addresses flag boolean:
